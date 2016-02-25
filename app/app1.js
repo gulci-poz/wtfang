28d8853a608e2b7cdaaa31c654907d8d1ec11f55
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2016-02-25 05:31:17 +0100

    2016-02-25 dependency injection

--- app/app1.js
+++ app/app1.js
@@ -9,3 +9,36 @@
 // angular dopuszcza też data-ng-reply (zgodne z html5)
 
 console.log($("h1").attr("ng-reply"));
+
+// dependency injection - przekazanie funkcji obiektu
+// angular wykorzystuje DI (kontrolery i inne instancje)
+
+var Person = function (firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+}
+
+/*
+function logPerson () {
+    // zmienna john jest zależnością funkcji logPerson (funkcja jest zależna od zmiennej)
+    // w razie jakiejkolwiek zmiany w obiekcie john, będzie trzeba dokonać zmian wewnątrz funkcji logPerson
+    var john = new Person("John", "Doe");
+    console.log(john);
+}
+
+logPerson();
+*/
+
+// podejście z DI
+
+function logPerson (person) {
+    console.log(person);
+}
+
+// funkcja logPerson nie jest zależna od obiektu john
+// ten obiekt może być stworzony w inny sposób, np. za pomocą danych wyciągniętych z bazy dancyh
+// funkcji logPerson to nie interesuje, zmiany w jej ciele nie są konieczne
+// wstrzykujemy zależność - przekazujemy obiekt
+
+var john = new Person("John", "Doe")
+logPerson(john);
